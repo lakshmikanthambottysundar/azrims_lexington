8ede6e941cd7e5478ad3af80048ccb68ad415ba1
blaxmiprasanna <blaxmiprasanna@yahoo.com>
2024-10-01 22:22:03 -0500

    Add two number function is added in the project

--- src/test/java/com/autozone/lexington/SecondClass.java
+++ src/test/java/com/autozone/lexington/SecondClass.java
@@ -1,10 +1,16 @@
 package com.autozone.lexington;
 
+import jdk.internal.org.jline.terminal.TerminalBuilder;
+
 public class SecondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the second class");
+		System.out.println("The sum of two numbers "+ addTwoNumber(10,12));
 	}
-
+	public static int addTwoNumber(int a, int b){
+		int c = a + b;
+		return c;
+	}
 }
